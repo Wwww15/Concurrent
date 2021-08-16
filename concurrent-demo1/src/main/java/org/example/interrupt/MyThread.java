6c25437d971a21d9b0aed36685ab04f70b230dbf
文志勋 <1006380802@qq.com>
2021-08-16 10:03:31 +0800

    1.守护线程测试

--- concurrent-demo1/src/main/java/org/example/interrupt/MyThread.java
+++ concurrent-demo1/src/main/java/org/example/interrupt/MyThread.java
@@ -8,8 +8,21 @@ public class MyThread extends Thread{
     @Override
     public void run() {
        while(true) {
-           boolean  interrupted = isInterrupted();
-           System.out.println("中断标记："+interrupted);
+           int i = 0;
+           while (true) {
+               boolean interrupted = isInterrupted();
+               System.out.println("中断标记：" + interrupted);
+               ++i;
+               if (i > 200) { // 检查并重置中断标志。
+                   boolean interrupted1 = Thread.interrupted();
+                   System.out.println("重置中断状态：" + interrupted1);
+                   interrupted1 = Thread.interrupted();
+                   System.out.println("重置中断状态：" + interrupted1);
+                   interrupted = isInterrupted();
+                   System.out.println("中断标记：" + interrupted);
+                   return;
+               }
+           }
        }
     }
 }
--- concurrent-demo1/src/main/java/org/example/interrupt/ThreadMain.java
+++ concurrent-demo1/src/main/java/org/example/interrupt/ThreadMain.java
@@ -2,8 +2,14 @@ package org.example.interrupt;
 
 public class ThreadMain {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        MyThread2 myThread = new MyThread2();
+        myThread.start();
+        Thread.sleep(10000);
+        myThread.interrupt();
+        Thread.sleep(3000);
+        System.out.println("main中断状态检查-1：" + myThread.isInterrupted());
+        System.out.println("main中断状态检查-2：" + myThread.isInterrupted());
     }
 }
 
